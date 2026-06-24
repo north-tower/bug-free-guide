52f8758323ea76ba0c262f9c7e61eead024b8c63
north-tower <muneshmyke@gmail.com>
2026-06-24 09:12:55 +0300

    v1

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,33 @@
+import type { SchemaTypeDefinition } from "sanity";
+import profile from "./profile";
+// import achievement from "./achievement";
+// import blog from "./blog";
+// import certification from "./certification";
+// import contact from "./contact";
+// import education from "./education";
+// import experience from "./experience";
+// import navigation from "./navigation";
+
+// import project from "./project";
+// import service from "./service";
+// import siteSettings from "./siteSettings";
+// import skill from "./skill";
+// import testimonial from "./testimonial";
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [
+    profile,
+    // project,
+    // skill,
+    // experience,
+    // education,
+    // testimonial,
+    // certification,
+    // achievement,
+    // blog,
+    // service,
+    // contact,
+    // siteSettings,
+    // navigation,
+  ],
+};
